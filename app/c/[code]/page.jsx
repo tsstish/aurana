117ed746336ad0964f918dc8ac9a9b22029e1eac
tsstish <tss.tish.tss@gmail.com>
2025-09-24 15:14:56 +0300

    Update page.jsx

--- app/c/[code]/page.jsx
+++ app/c/[code]/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 import { useParams } from "next/navigation";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../../lib/supabase";
 import { useEffect, useState } from "react";
 
 export default function ClientPage(){
